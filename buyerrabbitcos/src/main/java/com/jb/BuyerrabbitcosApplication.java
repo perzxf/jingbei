4bb58e80cd99519f8e0b95f8477a266fa199b1df
zhangchuanfei <963726646@qq.com>
2018-12-19 13:34:25 +0800

    提交买家项目

---
+++ buyerrabbitcos/src/main/java/com/jb/BuyerrabbitcosApplication.java
@@ -0,0 +1,14 @@
+package com.jb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerrabbitcosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerrabbitcosApplication.class, args);
+    }
+
+}
+
---
+++ buyerrabbitcos/src/main/java/com/jb/controller/BuyerController.java
@@ -0,0 +1,16 @@
+package com.jb.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+@Controller
+public class BuyerController {
+    @RabbitListener(queues = "buyqueue")
+    public void listenerBookMessage(String message){
+        Map map = JSON.parseObject(message, Map.class);
+        System.out.println("message = [" + map.toString() + "]");
+    }
+}
