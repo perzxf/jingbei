da51cf66b2efdd7a00a6657b1779338de2e7764a
zhangchuanfei <963726646@qq.com>
2018-12-19 13:35:00 +0800

    提交买家项目

---
+++ buyerrabbitpri/src/main/java/com/jb/BuyerrabbitpriApplication.java
@@ -0,0 +1,14 @@
+package com.jb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerrabbitpriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyerrabbitpriApplication.class, args);
+    }
+
+}
+
